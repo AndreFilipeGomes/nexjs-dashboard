515261c89d5e30cba153018f42e7204a7d28a2ef
Andre Gomes <andrefilipepereiragomes@gmail.com>
2025-01-10 16:56:48 +0000

    Initial implementation of search engine

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,10 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({
+  placeholder,
+}: Readonly<{ placeholder: string }>) {
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
